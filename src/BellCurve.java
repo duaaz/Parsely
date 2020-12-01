19d141bf56c917e923af371c491f3fc09c898882
Duaa Zaheer <41652959+duaaz@users.noreply.github.com>
2020-11-30 22:25:16 -0800

    hello

---
+++ src/BellCurve.java
@@ -0,0 +1,61 @@
+import java.awt.Font;
+import java.util.HashSet;
+
+public class BellCurve {
+	//you will need to install the stndDev pack from Princeton 
+	//instructions will be included in the spec
+	private double min;
+	private double max;
+	private double avg;
+	private double stndDev;
+	
+	public BellCurve(double miu, double sigma) {
+		avg = miu;
+		stndDev = sigma;
+		min = avg-(3*stndDev);
+		max = avg+(3*stndDev);
+	}
+	
+	 public void plot() {
+		 	StdDraw.setCanvasSize(600, 800);
+	        StdDraw.setXscale(min, max);
+	        StdDraw.setYscale(0, 0.1);
+	        StdDraw.text(avg, 0.01, "Candidate Total Points");
+	        StdDraw.setPenRadius(0.015);
+	        StdDraw.setPenColor(153, 204, 153);
+	        for (double x = min-10; x <= max+10; x += 0.1) {
+	            StdDraw.point(x, 0.015+pdf(x,avg,stndDev));
+	            if(Math.abs(x%stndDev) < 0.1) {
+	            	StdDraw.text(x, 0.013, (int)(x) + " ");
+	            }
+	        }
+	 }
+	 
+	 public void plotIndvCandidate(double points) {
+		 plot();
+		 StdDraw.setPenColor(StdDraw.BLACK);
+		 Font font = new Font("Arial", Font.BOLD, 50);
+		 StdDraw.setFont(font);
+		 StdDraw.text(points, 0.015+pdf(points,avg,stndDev), "x");
+		 Font font2 = new Font("Arial", Font.ITALIC, 16);
+		 StdDraw.setFont(font2);
+		 StdDraw.text(points, 0.0125+pdf(points,avg,stndDev), "You are here");
+	 }
+	 
+	 //return pdf(x) = standard Gaussian pdf
+	 public double pdf(double x) {
+	        return Math.exp(-x*x/2)/ Math.sqrt(2 * Math.PI);
+	    }
+	    
+	 //return return Gaussain pdf with consideration of the average and mean 
+	 public double pdf(double x, double avg, double stndDev) {
+	        return pdf((x - avg)/stndDev)/ stndDev;
+	    }
+	 
+	 //get rid of this at the end
+	 public static void main(String[] args) {
+	        BellCurve newOne1 = new BellCurve(105, 19);
+	        //newOne1.plot();
+	        newOne1.plotIndvCandidate(124);
+	 }
+}
---
+++ src/Resume.java
@@ -0,0 +1,146 @@
+//Duaa Zaheer, Davin Kyi, Allison Li
+//11/20/2020
+//Resume Reader 
+
+
+//Method description  
+/*
+This is a program that will be able to give you a short anaylsis of a resume in
+which you will be looking at. This will give a company a quick anaylsis on which
+resume in which they looked at is most highly ranked, in other words, which one has
+more points when compared to the other resumes given. Also, the program can also 
+give you the amount of experience you've had with a certain keyword, such as java.
+This program will also allow you to see if the basic skills, such as Java are given
+in the resume, and this is done by checking to see if it is contained within the 
+resume given. Last and not least, it will give you all the words that are contained
+within the resume you are looking at.  
+
+*/ 
+
+import java.util.*;
+import java.io.*;
+
+public class Resume {
+   /*
+   //imagine if a person say present 
+   private static final String present = "present";
+   */
+   //name of the person who wrote the resume  
+   private String name;
+   //this most likely will just tell you all the words it contains
+   private Set<String> keyWords;
+   //<Keyword, Experience>
+   private Map<String, Integer> experience; 
+   //these are the months in the years, and they are abbreviated  
+   private ArrayList<String> months;
+   //these are helper fields for the methods, to help store the data 
+   private Map<String, Integer> time;
+   private Scanner scanner;
+   //percentile of the cainidate relative to other cainidates  
+   private double percentile;
+   //this is the file of the resume  
+   private File resumeData;  
+   
+   //i think we are going to pass in a file instead perhaps  
+   public Resume(File file) throws FileNotFoundException {
+      this.name = "";
+      this.resumeData = file;
+      scanner = new Scanner(file);
+      //initalizing the Set of keywords in which we will be returning later 
+      this.keyWords = new HashSet<String>();
+      while(scanner.hasNext()) {
+         String word = scanner.next();
+         this.keyWords.add(word);
+      }
+      this.experience = new HashMap<String, Integer>();	      
+      this.time = new HashMap<String, Integer>();
+      //first one is 11/30/2015, Nov 27, 2002 
+      String [] months = {"jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"}; 
+      for (int i = 0; i < months.length; i++) {
+         this.months.add(months[i]);
+      }
+   }
+   
+   //maybe we will assume that the person's name is the first two words in the resume  
+   //this will return the name of the person wo wrote the resume  
+   public String getName() {
+      //we have to determine how to get the name from the resume  
+      //THIS WILL BE THE FIRST LINE OF THE METHOD, OR THE EMPLOYER WILL PUT IN THE NAME IN HERE  
+      return this.name;
+   }
+   
+   public Set<String> getKeyWords() {
+      return keyWords;
+   }
+   
+   //this will check to see if all of the words are contained within your keyWords needed map  
+   public boolean qualify(Set<String> neededWords) {
+     return keyWords.containsAll(neededWords);
+   }
+   
+   //this will allow you to get all of the individual keywords amount of experience
+   //by giving you the total time in which has shown up with the word   
+   //THIS IS THE MAIN THING WE NEED TO GET TO WORK, SO FAR THE BASE STRUCTURE GIVEN SO FAR
+   //SHOULD WORK, WE MIGHT NEED TO DEBUG IF THIS DOSEN'T WORK,
+   public Map<String, Integer> getExperience() throws FileNotFoundException {
+      scanner = new Scanner(this.resumeData);
+      //this will work if we are assuming that the first 2 words are going to be the 
+      //name of the person in which we are talking about  
+      this.name = "";
+      String firstName = scanner.next();
+      String lastName = scanner.next();
+      this.name = firstName + " " + lastName; 
+      while(scanner.hasNextLine()) {
+         int totalMonths = 0;
+         String line = scanner.nextLine();
+         Scanner newScan = new Scanner(line);
+         //we only want to put the keyword once into the map
+         //and then from that one line, give that one keyword a time 
+         //this will allow me to only change the times on the keywords found on
+         //the one line 
+         Set<String> foundWords = new HashSet<String>();  
+         while (newScan.hasNext()) {
+            //this is a starter for the reading in of the time, but
+            //this is going to be trickier than i thought  
+            String word = newScan.next();
+            foundWords.add(word);
+            if (months.contains(word.substring(0, 3))) {
+               String startMonth = newScan.next();
+               int startYear = newScan.nextInt();
+               //we will think about what to do with a to or a - (always a -, or can it be a to/from, and etc.)  
+               newScan.next(); //throws away the dash in the text 
+               String endMonth = newScan.next().substring(0, 3);
+               int endYear = newScan.nextInt();
+               totalMonths += (endYear - startYear) * 12;
+               //approx 30 days for each month, we can figure this out later as well
+               totalMonths += months.indexOf(endMonth) - months.indexOf(startMonth);
+            } 
+        }
+        for (String keyWord : foundWords) {
+           //for each of the words that were in that one line
+           //we will add the additional time
+           //but, we do not want to add the time to every word found,
+           //unless if it was on the line  
+           experience.put(keyWord, time.get(keyWord) + totalMonths);
+        }
+      }
+      return experience;
+   }
+   
+   // we want to figure out, are we going to be returning how many sets...
+   public Map<String, Set<String>> containsWanted(Map<String, Set<String>> keyWord) {
+      Map<String, Set<String>> foundKeyWords = new HashMap<String, Set<String>>();
+      for (String type : keyWord.keySet()) {
+         Set<String> possibleKeyWords = keyWord.get(type);
+         for (String possibleKeyWord : possibleKeyWords) {
+            if (keyWords.contains(possibleKeyWord)) {
+               if (!foundKeyWords.containsKey(type)) {
+                  foundKeyWords.put(type, new HashSet<String>());
+               }
+               foundKeyWords.get(type).add(possibleKeyWord);
+            }
+         } 
+      }
+      return foundKeyWords;     
+   }
+}
